ec2891d65e86aedce4afb992fa1fcb3f7093eb33
NandK <keitaro.noda@gmail.com>
2025-03-18 17:11:53 +0900

    -

--- convolve.cpp
+++ convolve.cpp
@@ -1,108 +1,5 @@
 #pragma once
-
-#ifdef __CUDA_ARCH__
-
-#include <thrust/complex.h>
-#include <cuda_runtime.h>
-
-using namespace std;
-using COMP = thrust::complex<double>;
-
-__global__ void cfft(COMP* v, int n, int siz, bool inv) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    int l = 1<<siz;
-    int hl = l>>1;
-    int g = id/hl;
-    int pos = g*l + id%hl;
-    if (pos+hl< n) {
-        COMP a = v[pos], b = v[pos+hl];
-        double angle = 2.0*M_PI*(id%hl)/l;
-        if (!inv) angle *= -1;
-        COMP phase = COMP(cos(angle), sin(angle));
-        b *= phase;
-        v[pos] = a+b;
-        v[pos+hl] = a-b;
-    }
-}
-
-__global__ void kbit_rev(COMP* v, int m, int n) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id >= n) return;
-    int j = 0, x = id;
-    for (int i = 0; i < m; i++) j = (j << 1) | (x & 1), x >>= 1;
-    if (j > id) {
-        COMP tmp = v[id];
-        v[id] = v[j];
-        v[j] = tmp;
-    }
-}
-
-__global__ void kmul(COMP* v, COMP *u, int n) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id >= n) return;
-    v[id] *= u[id];
-}
-
-__global__ void kdiv(COMP* v, int m, int n) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id >= n) return;
-    v[id] /= m;
-}
-
-__global__ void rtoi(COMP* v, int* u, int n) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id >= n) return;
-    u[id] = __double2int_rn(v[id].real());
-}
-
-__global__ void itor(int* v, COMP* u, int n) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id >= n) return;
-    u[id] = COMP(__int2double_rn(v[id]), 0.0);
-}
-
-void fft(COMP* v, int n, bool inv=false) {
-    int m = 0; while ((1<<m)<n) m++;
-    int blockSize = 32;
-    int numBlocks = (n/2 + blockSize - 1) / blockSize;
-    kbit_rev<<<numBlocks*2, blockSize>>>(v, m, n); // bit反転はn個のコアで行う必要がある
-    for (int siz = 1; (1<<siz) <= n; siz++) cfft<<<numBlocks, blockSize>>>(v, n, siz, inv);
-    return;
-}
-
-int* convolve(int* f_, int* g_, int siz) {
-    int n = siz*2;
-    int blockSize = 32;
-    int numBlocks = (n + blockSize - 1) / blockSize;
-
-    COMP *f, *g;
-    cudaMalloc(&f, sizeof(COMP)*n);
-    cudaMalloc(&g, sizeof(COMP)*n);
-    // 配列全体をゼロ初期化
-    cudaMemset(f, 0, sizeof(COMP) * n);
-    cudaMemset(g, 0, sizeof(COMP) * n);
-    itor<<<numBlocks, blockSize>>>(f_, f, siz);
-    itor<<<numBlocks, blockSize>>>(g_, g, siz);
-
-    fft(f, n);
-    fft(g, n);
-
-    kmul<<<numBlocks, blockSize>>>(f, g, n);
-    fft(f, n, true);
-    kdiv<<<numBlocks, blockSize>>>(f, n, n);
-
-    int* r;
-    cudaMalloc(&r, sizeof(int)*n);
-    rtoi<<<numBlocks, blockSize>>>(f, r, n);
-    cudaFree(f); cudaFree(g);
-
-    return r;
-}
-
-#else
-
-
-#pragma once
+#include <algorithm>
 #include <complex>
 #include <vector>
 #include <math.h>
@@ -163,10 +60,7 @@ vector<int> convolve(vector<int> f_, vector<int> g_) {
     vector<int> ret(n);
     for (int i = 0; i < n; i++) ret[i] = round(h[i].real() / n);
 
-    vector<int> r(f_.size());
-    for (int i = 0; i < f_.size(); i++) r[i] = ret[f_.size()/2+i];
-    return r;
+    while (ret.back() == 0) ret.pop_back();
+
+    return ret;
 }
-
-
-#endif
--- main.cpp
+++ main.cpp
@@ -2,55 +2,48 @@
 #include <iostream>
 using namespace std;
 
-// 1/sqrt(10005)
-BigDecimal invrt(BigDecimal a) {
-    BigDecimal x, _1, _5;
-    // xの初期値は0.00999
-    x.set(x.siz/2-3, 9);
-    x.set(x.siz/2-4, 9);
-    x.set(x.siz/2-5, 9);
-    _1.set(_1.siz/2, 1); // 1
-    _5.set(_5.siz/2-1, 5); // 0.5 = 1/2
-    for (int k = 1; k <= a.siz; k <<= 1) {
-        x = x + _5 * x * (_1 - a * x * x);
-    }
-    return x;
-}
+#define C 64320
+#define C3_OVER24 11087327232000LL
 
-// binary splitting method
-void binary_split(BigDecimal* x, BigDecimal *y, BigDecimal *z, int n) {
-    for (int i = 1; (1<<i) <= n; i++) {
-        for (int j = 0; j < n; j += (1<<i)) {
-            int k = 1<<(i-1);
-            BigDecimal nx = x[j] * x[j+k];
-            BigDecimal ny = x[j+k] * y[j] + y[j+k] * z[j];
-            BigDecimal nz = z[j] * z[j+k];
-            x[j] = nx;
-            y[j] = ny;
-            z[j] = nz;
+struct PQT {
+    dcm p, q, t;
+    PQT(dcm a, dcm b, dcm c) : p(a), q(b), t(c) {}
+};
+
+dcm chudnovsky_bs(int digits) {
+    auto bs = [&](auto f, int a, int b)->PQT {
+        dcm Pab, Qab, Tab;
+        if (b - a == 1) {
+            if (a == 0) Pab = Qab = 1;
+            else {
+                Pab = (6*a-5)*(2*a-1)*(6*a-1);
+                Qab = a*a*a*C3_OVER24;
+            }
+            Tab = Pab * (13591409 + 545140134*a);
+            if (a & 1) Tab.sgn = true;
         }
-    }
+        else {
+            int m = (a + b) / 2;
+            auto [Pam, Qam, Tam] = f(f, a, m);
+            auto [Pmb, Qmb, Tmb] = f(f, m, b);
+            Pab = Pam * Pmb;
+            Qab = Qam * Qmb;
+            Tam = Qmb * Tam + Pam * Tmb;
+        }
+        return PQT(Pab, Qab, Tab);
+    };
+
+    double Digits_per_term = log10(C3_OVER24/72);
+    int n = digits / Digits_per_term + 1;
+    cout << n << endl;
+
+    PQT Z = bs(bs, 0, n);
+    vector<int> tmp(digits+1);
+    tmp.back() = 1;
+    dcm A(4270934400LL);
+    return A*Z.q*invrt(10005, digits);
 }
-
-
 
 int main() {
-    int n = 1<<5;
-    BigDecimal A = 13591409, B = 545140134, C = 640320;
-    BigDecimal x[n], y[n], z[n];
-    for (int i = 0; i < n; i++) {
-        BigDecimal k = 2*i, kk = 2*i+1;
-        BigDecimal x1 = k*k*k*C*C*C/24, x2 = kk*kk*kk*C*C*C/24, y1 = A+B*k, y2 = A+B*kk, z1 = (k*6+1)*(k*2+1)*(k*6+5), z2 = (kk*6+1)*(kk*2+1)*(kk*6+5);
-        if (i == 0) x1 = 1;
-        if (i == n-1) z2 = 0;
-        x[i] = x1*x2;
-        y[i] = x2*y1-y2*z1;
-        z[i] = z1*z2;
-    }
-    binary_split(x, y, z, n);
-    BigDecimal a = 4270934400ll;
-
-    cout << a*x[0]*invrt(10005)/y[0] << endl;
+    cout << chudnovsky_bs(20) << endl;
 }
-
-
